3911f2ad3be0b9bbdd1df523555a780c7ca1e5c1
dawnny <dawnnyho@gmail.com>
2022-06-05 15:26:07 -0400

    add google login v6051

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,37 @@
+import {useEffect} from 'react';
+// const { MongoClient } = require("mongodb");
+// const path = require("path");
+// require("dotenv").config({path: "../server/.env"});
 
+//create env file and import it
+// const { CLIENT_ID,CLIENT_SECRET } = process.env;
 
 function App() {
+  const handlecallback = (response)=>{
+   console.log("Encode token" + response.credential)
+  }
+  useEffect(()=>{ 
+    // global google
+    google.accounts.id.initialize({
+      client_id: "474484987872-8j83f3q0uaqh30ml2tke3rhqujbnr4v8.apps.googleusercontent.com",
+      callback: handlecallback
+    })
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline", size: "large"}
+    )
+
+  },[])
+
+
   return (
-    <div>
+    <div className='App'>
+      <div id="signInDiv"></div>
+
+
       
-         <h1> Learn React</h1>
+         <h1> React</h1>
       
     </div>
   );
