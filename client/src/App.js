ef476c8289e71166ea898902045558a85945b5ca
dawnny <dawnnyho@gmail.com>
2022-06-05 17:04:06 -0400

    v6052

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import jwt_decode from 'jwt-decode';
 // const { MongoClient } = require("mongodb");
 // const path = require("path");
 // require("dotenv").config({path: "../server/.env"});
@@ -7,11 +8,24 @@ import {useEffect} from 'react';
 // const { CLIENT_ID,CLIENT_SECRET } = process.env;
 
 function App() {
+
+  const [user, setUser] = useState({})
+
   const handlecallback = (response)=>{
-   console.log("Encode token" + response.credential)
+  //  console.log("Encode token" + response.credential)
+   const userObj = jwt_decode(response.credential)
+   console.log(userObj)
+  //  console.log("test")
+    setUser(userObj)
+    document.getElementById("signInDiv").hidden=true;
+  }
+
+  const handleSignout = ()=>{
+    setUser({});
+    document.getElementById("signInDiv").hidden=false;
   }
   useEffect(()=>{ 
-    // global google
+    /* global google */
     google.accounts.id.initialize({
       client_id: "474484987872-8j83f3q0uaqh30ml2tke3rhqujbnr4v8.apps.googleusercontent.com",
       callback: handlecallback
@@ -26,14 +40,23 @@ function App() {
 
 
   return (
+    
     <div className='App'>
       <div id="signInDiv"></div>
-
-
+      {Object.keys(user).length !=0 &&
+       <button onClick={(e)=>handleSignout(e)}>Sign Out</button>
+      }
+     
       
-         <h1> React</h1>
       
-    </div>
+      {user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
+
+    </div>          
   );
 }
 
