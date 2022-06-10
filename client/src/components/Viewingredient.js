a459ccf319468c319b13a7a328822c6ae04156e0
dawnny <dawnnyho@gmail.com>
2022-06-09 23:57:40 -0400

    v6096

--- client/src/components/Viewingredient.js
+++ client/src/components/Viewingredient.js
@@ -24,18 +24,25 @@ const Viewingredient = () => {
   return (
   <>
  
-    <h1>View Major Ingredients</h1>
+    <h1>View Major Ingredients(Link to Wiki)</h1>
     <br></br>
     {/* {recipes&&recipes.length >0 ? ( */}
     
      { ingredients.data?.map((el)=>{
         // console.log("el",el)
-        
+        let url="https://en.wikipedia.org/wiki/"+el.name
+        let urlc="https://en.wikipedia.org/wiki/"+el.cname
+
     return(
         <Wrapper>
+           {/* <a target="_blank" href="https://en.wikipedia.org/wiki/Canada">Canada</a> */}
          <Div1>{el.number}</Div1>
-         <Div2>{el.name}</Div2>
-         <Div3>{el.cname}</Div3>
+         
+         {/* <Div2>{el.name}</Div2> */}
+         <Div2><a target="_blank" href= {url}>{el.name}</a></Div2>
+         <Div3><a target="_blank" href= {urlc}>{el.cname}</a></Div3>
+
+         {/* <Div3>{el.cname}</Div3> */}
          <Div4>{el.function}</Div4>
          </Wrapper>
         )     
@@ -49,12 +56,14 @@ export default Viewingredient
 
 const Wrapper = styled.div`
   display: grid;
-  grid-template-rows: 80 px;
+  grid-template-rows: 30px ;
   grid-template-columns: 30px 150px 60px 250px;
-  gap: 25px 15px;
+  gap: 10px 20px;
   background: #fff;
   position: relative;
   margin-left: 50px;
+  border-bottom: 10px;
+  /* margin-bottom: 10px; */
 `;
 
 const Div1 =styled.div`
